b09d4f627e363e443d67ab2e350661e5e367bc11
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-08-27 15:14:44 +0530

    define campground schema

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var express       = require('express'),
+    app           = express(),
+    bodyParser    = require('body-parser'),
+    mongoose      = require('mongoose');
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+//Schema setup
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 var campgrounds = [
   {
