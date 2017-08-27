25d64f2985245563363a8a2a1c1db2bc8b305921
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-08-27 09:09:29 +0530

    populate campgrounds page with new campground

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+var campgrounds = [
+  {
+    name: "Woods Camp",
+    image: "https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?w=940&h=650&auto=compress&cs=tinysrgb"
+  },
+  {
+    name: "Hills Camp",
+    image: "https://images.pexels.com/photos/14287/pexels-photo-14287.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+  },
+  {
+    name: "Desert Camp",
+    image: "https://images.pexels.com/photos/260593/pexels-photo-260593.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+  }
+]
+
 //landing page
 app.get('/', function(req, res){
   res.render('landing');
@@ -12,27 +27,16 @@ app.get('/', function(req, res){
 
 //campgrounds page
 app.get('/campgrounds', function(req, res){
-  var campgrounds = [
-    {
-      name: "Woods Camp",
-      image: "https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?w=940&h=650&auto=compress&cs=tinysrgb"
-    },
-    {
-      name: "Hills Camp",
-      image: "https://images.pexels.com/photos/14287/pexels-photo-14287.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
-    },
-    {
-      name: "Desert Camp",
-      image: "https://images.pexels.com/photos/260593/pexels-photo-260593.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
-    }
-  ]
-
   res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 //campgrounds page with added campground
 app.post('/campgrounds', function(req, res){
-  res.send("You hit the post route!");
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds'); //default redirect as a get request
 });
 
 //new campground form page
