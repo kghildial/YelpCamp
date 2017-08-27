32308521e2615e9b1e58909186b9fdfd757fb4d8
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-08-27 15:25:56 +0530

    remove array

--- app.js
+++ app.js
@@ -29,33 +29,6 @@ Campground.create(
     }
   });
 
-var campgrounds = [
-  {
-    name: "Woods Camp",
-    image: "https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?w=940&h=650&auto=compress&cs=tinysrgb"
-  },
-  {
-    name: "Hills Camp",
-    image: "https://images.pexels.com/photos/14287/pexels-photo-14287.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
-  },
-  {
-    name: "Desert Camp",
-    image: "https://images.pexels.com/photos/260593/pexels-photo-260593.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
-  },
-  {
-    name: "Woods Camp",
-    image: "https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?w=940&h=650&auto=compress&cs=tinysrgb"
-  },
-  {
-    name: "Hills Camp",
-    image: "https://images.pexels.com/photos/14287/pexels-photo-14287.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
-  },
-  {
-    name: "Desert Camp",
-    image: "https://images.pexels.com/photos/260593/pexels-photo-260593.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
-  }
-]
-
 //landing page
 app.get('/', function(req, res){
   res.render('landing');
