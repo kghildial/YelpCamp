8cd741c81d0a55d5f04464159c173cc9970e7a95
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-08-28 18:05:28 +0530

    update campgroundSchema

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set('view engine', 'ejs');
 //Schema setup
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
