1c9efdda9a8e11b9d4267b5d6699d6e8fb00f911
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-08-30 11:08:57 +0530

    rename campgrounds to index

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/campgrounds', function(req, res){
       console.log(err);
     }
     else{
-      res.render('campgrounds', {campgrounds: allCampgrounds});
+      res.render('index', {campgrounds: allCampgrounds});
     }
   });
 });
