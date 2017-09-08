f9be9184e9f17c3503c331b1fc2fd75db4572ed8
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-09-08 13:42:10 +0530

    add comment post route

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express       = require('express'),
     bodyParser    = require('body-parser'),
     mongoose      = require('mongoose'),
     Campground    = require("./models/campground"),
-    seedDB        = require("./seeds");
+    seedDB        = require("./seeds"),
+    Comment       = require("./models/comment");
 
 seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -70,6 +71,27 @@ app.get('/campgrounds/:id/comments/new', function(req, res){
     }
     else{
       res.render('comments/new', {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+      redirect("/campgrounds");
+    }
+    else {
+      Comment.create(req.body.comment, function(err, comment){
+        if(err){
+          console.log(err)
+        }
+        else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      })
     }
   });
 });
