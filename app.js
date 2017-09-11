f3706edb01a365efb0767bab93f49e4585a81820
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-09-11 15:17:11 +0530

    update styles

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
 
 //landing page
 app.get('/', function(req, res){
