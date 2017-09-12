b1b4cb482340c12ce728e8268bec8c6847e4c82e
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-09-12 12:43:07 +0530

    update comment model

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes        = require("./routes/index");
 
-seedDB();
+// seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 
 var commentSchema = mongoose.Schema({
   text: String,
-  author: String
+  author: {
+    id:{
+      type: mongoose.Schema.Types.ObjectId, //type of the data
+      ref: "User" //model to which id refers ie. the User model
+    },
+    username: String
+  }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
