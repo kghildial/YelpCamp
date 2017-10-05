5dc4556e5f034cf5e12e018218d3d8a0a1fa4ca7
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2017-10-05 20:18:48 +0530

    add comment delete logic

--- routes/comments.js
+++ routes/comments.js
@@ -71,7 +71,14 @@ router.put("/:comment_id", function(req, res){
 
 //Comment Destroy route
 router.delete("/:comment_id", function(req, res){
-  res.send("This is the destroy comment route!!!");
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect("back");
+    }
+    else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
 });
 
 //check user logged in middleware
